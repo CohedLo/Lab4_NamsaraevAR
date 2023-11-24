d3547a4b9dd653751e787ee37514b1dda9d750e9
Amgalan03 <namsaraevar@mer.ci.nsu.ru>
2023-11-24 20:51:51 +0700

    Добавьте файлы проекта.

---
+++ Lab4/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("https://www.cyberforum.ru/csharp-beginners/thread2787061.html");
+
+        var comments = driver.FindElements(By.CssSelector("#post15280941 > tbody"));
+        Console.WriteLine(comments.Count);
+
+        foreach (var comment in comments)
+        {
+            var username = comment.FindElement(By.CssSelector("#post15280941 > tbody > tr:nth-child(1) > td > div:nth-child(1) > a")).Text; 
+            var commentText = comment.FindElement(By.CssSelector("#post_message_15280941")).Text;
+
+            Console.WriteLine($"Username: {username} - Comment: {commentText}");
+        }
+
+        driver.Quit();
+    }
+}
